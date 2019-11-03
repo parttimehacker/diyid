5d5f16383ea2b041c64ff2210b4b8c91c1d08445
Dave Wilson <parttimehacker@gmail.com>
2019-11-03 10:10:09 -0800

    added new import script

---
+++ import.script.sh
@@ -0,0 +1,17 @@
+#/usr/bin/bash
+#
+# create aliases
+echo "Install Mqtt and psutil"
+sudo pip3 install paho-mqtt
+sudo pip3 install psutil
+
+echo "Install Adafruit stuff"
+git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
+cd Adafruit_Python_GPIO
+sudo python3 setup.py install
+cd ..
+git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
+cd Adafruit_Python_SSD1306
+sudo python3 setup.py install
+cd ..
+
